7c2295e0ec18a1f7358c95a347e7aa46b92d6da4
zhanyl <turtemirova.zhanyl@gmail.com>
2020-08-03 10:27:35 +0600

    add other sections and gogle map

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.glider'), {
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      draggable: true,
-      arrows: {
-        prev: '.glider-prev',
-        next: '.glider-next'
-      },
-      responsive: [
-      {
-        // screens greater than >= 1024px
-        breakpoint: 1024,
-        settings: {
-          slidesToScroll: 1,
-          itemWidth: 250,
-          duration: 0.25
-        }
-      }
-    ]
-    })
-  })
-  
-  function create(htmlStr) {
-    var frag = document.createDocumentFragment(),
-        temp = document.createElement('div');
-    temp.innerHTML = htmlStr;
-    while (temp.firstChild) {
-        frag.appendChild(temp.firstChild);
-    }
-    return frag;
-  }
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 42.874860, lng: 74.612243};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
 
-var fragment = create('<div>Hello!</div><p>...</p>');
-// You can use native DOM methods to insert the fragment:
-document.body.insertBefore(fragment, document.body.childNodes[0]);
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.glider'), {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // draggable: true,
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+    scrollLock: false,
+    responsive: [
+    {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToScroll: 1,
+        itemWidth: 250,
+        duration: 0.25
+      }
+    }
+  ]
+  })
+  new Glider(document.querySelector('.glider--2'), {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // draggable: true,
+    arrows: {
+      prev: '.glider-prev--2',
+      next: '.glider-next--2'
+    },
+    scrollLock: false,
+    responsive: [
+    {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToScroll: 1,
+        itemWidth: 250,
+        duration: 0.25
+      }
+    }
+  ]
+  }) 
+  initMap();
+})
