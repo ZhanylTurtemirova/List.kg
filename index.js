d77b2a31eb15d3bd5f257670c225ee9f8c7cf750
zhanyl <turtemirova.zhanyl@gmail.com>
2020-08-03 21:35:40 +0600

    fix map

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: 42.874860, lng: 74.612243};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
+// function initMap() {
+//   // The location of Uluru
+//   var uluru = {lat: 42.874860, lng: 74.612243};
+//   // The map, centered at Uluru
+//   var map = new google.maps.Map(
+//       document.getElementById('map'), {zoom: 4, center: uluru});
+//   // The marker, positioned at Uluru
+//   var marker = new google.maps.Marker({position: uluru, map: map});
+// }
 
 window.addEventListener('load', function(){
   new Glider(document.querySelector('.glider'), {
