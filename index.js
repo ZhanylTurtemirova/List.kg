7f48fe3bdfa1009e34cbaef4410c039b683675bf
zhanyl <turtemirova.zhanyl@gmail.com>
2020-08-12 23:47:29 +0600

    fix styles, add slert page

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 window.addEventListener('load', function(){
-  new Glider(document.querySelector('.glider'), {
+  const slider1 = document.querySelector('.glider') ? document.querySelector('.glider')  : document.querySelector('.glider--2') 
+  const prevClass1 =  document.querySelector('.glider-prev') ? '.glider-prev' : '.glider-prev--2'
+  const nextClass1 =  document.querySelector('.glider-next') ? '.glider-next' : '.glider-next--2'
+  new Glider(slider1, {
     slidesToShow: 3,
     slidesToScroll: 1,
     // draggable: true,
     arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
+      prev: prevClass1,
+      next: nextClass1
     },
     scrollLock: false,
     responsive: [
@@ -20,13 +23,17 @@ window.addEventListener('load', function(){
     }
   ]
   })
-  new Glider(document.querySelector('.glider--2'), {
+
+  const slider2 = document.querySelector('.glider--2') ? document.querySelector('.glider--2') : document.querySelector('.glider') 
+  const prevClass2 =  document.querySelector('.glider-prev--2') ? '.glider-prev--2' : '.glider-prev'
+  const nextClass2 =  document.querySelector('.glider-next--2') ? '.glider-next--2' : '.glider-next'
+  new Glider(slider2, {
     slidesToShow: 3,
     slidesToScroll: 1,
     // draggable: true,
     arrows: {
-      prev: '.glider-prev--2',
-      next: '.glider-next--2'
+      prev: prevClass2,
+      next: nextClass2
     },
     scrollLock: false,
     responsive: [
